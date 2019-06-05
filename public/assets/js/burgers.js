b76003941a29893580108b02f8259a80f8463c75
Adam S <leavinit@gmail.com>
2019-06-04 22:02:31 -0500

    Added a reset button to move all burgers to uneaten as a luxury feature.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,8 +1,10 @@
 $(function() {
-    $(".change-devoured").on("click", function(event) {
+  $("#eaten button").toggleClass("btn-primary").css("color","white");  
+  $(".change-devoured").on("click", function(event) {
+
+      //builds a put request by getting the id and next devoured state from the data attrs
       var id = $(this).data("id");
       var newDevoured = $(this).data("newdevoured");
-  
       var newDevouredState = {
         devoured: newDevoured
       };
@@ -24,24 +26,33 @@ $(function() {
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
+      
       var newBurger = {
         burger_name: $("#burger").val().trim(),
         devoured: 0 
         //$("[name=devoured]:checked").val().trim()
       };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+      if($("#burger").val().trim() !== ""){  
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      }else{//ENDIF
+        alert("Please enter a burger name!")
+      }
     });
+    //handle the reset button.  moves all the burgers to uneaten
+    $("#resetBtn").on("click", function(event){
+      // alert('working');
+      $("#eaten button").trigger("click");
+    });
+    //
   });
   
