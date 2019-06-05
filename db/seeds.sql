bc5dbef7ed663ad2291a510fc7249d3a628a5769
Adam S <leavinit@gmail.com>
2019-06-04 23:50:06 -0500

    Saved fixed seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
 INSERT INTO burgers (burger_name) VALUES ('Cheddar and Onion ');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss Mushroom');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Egg on Top');
-INSERT INTO burgers (burger_name) VALUES ('Monster Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('Swiss Mushroom');
+INSERT INTO burgers (burger_name) VALUES ('Egg on Top');
